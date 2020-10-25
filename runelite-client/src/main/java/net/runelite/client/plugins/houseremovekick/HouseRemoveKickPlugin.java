f8bbe616fc63da9328072cb74ccdf90ed3f26a54
Jonathan Rider <slith93@gmail.com>
2020-10-25 17:47:50 -0400

    House Remove Kick plugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/houseremovekick/HouseRemoveKickPlugin.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * Copyright (c) 2018, Kamiel
+ * Copyright (c) 2019, Rami <https://github.com/Rami-J>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.houseremovekick;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.inject.Provides;
+import lombok.Getter;
+import net.runelite.api.*;
+import net.runelite.api.events.*;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.input.KeyManager;
+import net.runelite.client.menus.MenuManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+import java.util.*;
+
+@PluginDescriptor(
+	name = "Remove House Kick",
+	description = "Remove kick option from houses",
+	tags = {"house", "kick"},
+	enabledByDefault = false
+)
+public class HouseRemoveKickPlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private HouseRemoveKickConfig config;
+
+
+	@Subscribe
+	public void onClientTick(ClientTick clientTick)
+	{
+		// The menu is not rebuilt when it is open, so don't swap or else it will
+		// repeatedly swap entries
+		if (client.getGameState() != GameState.LOGGED_IN || client.isMenuOpen())
+		{
+			return;
+		}
+
+		MenuEntry[] menuEntries = client.getMenuEntries();
+		List<MenuEntry> entryList = new ArrayList<MenuEntry>();
+		for (MenuEntry entry : menuEntries)
+		{
+			if(!entry.getOption().equals(config.getRemoveActions())){
+				entryList.add(entry);
+			}
+		}
+		MenuEntry[] newEntries = new MenuEntry[entryList.size()];
+		newEntries = entryList.toArray(newEntries);
+
+		//set and regrab for normal runelite functionality
+		client.setMenuEntries(newEntries);
+	}
+}
